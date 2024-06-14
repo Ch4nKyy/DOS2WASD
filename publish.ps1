9947bab58cf1ae34f163029bd1fba4ecdb26243d
Ch4nKyy <ch4nkyy@gmail.com>
2024-06-14 18:35:56 +0200

    Change folder structure to support Vortex

--- publish.ps1
+++ publish.ps1
@@ -11,13 +11,12 @@ Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -C
 & cmake -B $PSScriptRoot/build -S $PSScriptRoot --preset=REL -DPLUGIN_MODE:BOOL=TRUE
 & cmake --build $PSScriptRoot/build --config Release
 
-Remove-Item $PSScriptRoot\package\bin -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
+Remove-Item $PSScriptRoot\package\DefEd -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
 Remove-Item $PSScriptRoot\package\*.zip -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
-New-Item -ItemType Directory -Path $PSScriptRoot\package\bin | Out-Null
-New-Item -ItemType Directory -Path $PSScriptRoot\package\bin\NativeMods | Out-Null
-Copy-Item $PSScriptRoot\extern\SDL2\SDL2*.dll $PSScriptRoot\package\bin\
-Copy-Item $PSScriptRoot\extern\NativeModLoader\*.dll $PSScriptRoot\package\bin\
-Copy-Item $PSScriptRoot\build\Release\* $PSScriptRoot\package\bin\NativeMods\
-Copy-Item $PSScriptRoot\package\*.toml $PSScriptRoot\package\bin\NativeMods\
-& "C:\Program Files\7-Zip\7z.exe" a -tzip $PSScriptRoot\package\DOS2WASD.zip $PSScriptRoot\package\bin
-Remove-Item $PSScriptRoot\package\bin -Force -Recurse  -ErrorAction:SilentlyContinue | Out-Null
+New-Item -ItemType Directory -Path $PSScriptRoot\package\DefEd\bin\NativeMods | Out-Null
+Copy-Item $PSScriptRoot\extern\SDL2\SDL2*.dll $PSScriptRoot\package\DefEd\bin\
+Copy-Item $PSScriptRoot\extern\NativeModLoader\*.dll $PSScriptRoot\package\DefEd\bin\
+Copy-Item $PSScriptRoot\build\Release\* $PSScriptRoot\package\DefEd\bin\NativeMods\
+Copy-Item $PSScriptRoot\package\*.toml $PSScriptRoot\package\DefEd\bin\NativeMods\
+& "C:\Program Files\7-Zip\7z.exe" a -tzip $PSScriptRoot\package\DOS2WASD.zip $PSScriptRoot\package\DefEd
+Remove-Item $PSScriptRoot\package\DefEd -Force -Recurse  -ErrorAction:SilentlyContinue | Out-Null
